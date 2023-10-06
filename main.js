2fbe5218955cdb2f8d8c4ad46fb0e2517b2eda6b
kishanviswakarma83@gmail.com <kishanviswakarma83@gmail.com>
2023-10-07 02:56:36 +0530

    Sound effect

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ let triviabox = document.querySelector(".container");
 let questiontext;
   let rightans;
   let wrongans;
+  let sound = true;
   let buttons = document.querySelectorAll(".ans");
-function fetchData(url) {
-  return fetch(url)
+function fetchData(link) {
+   url = `https://opentdb.com/api.php?amount=1&category=${y.value}&difficulty=${x.value}&type=multiple`;
+   console.log(url)
+  return fetch(link)
     .then(async (response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -18,8 +21,10 @@ function fetchData(url) {
       return data.results[0];
     });
 };
- 
+let audio = document.getElementById("audio")
+
 function start(){
+  soundplay("flip");
   document.querySelector(".home").style.display = "none";
   triviabox.style.display = "flex"
   triviabox.style.animation = "flipIn 1s ease-in";
@@ -27,6 +32,7 @@ function start(){
  let ansbox = document.querySelectorAll(".ans h1")
  
   fetchData(url).then((data) => {
+    
     console.log(data.incorrect_answers)
   questiontext =  resolve(data.question);
   rightans = resolve(data.correct_answer);
@@ -54,6 +60,7 @@ buttons.forEach((x) => {
 
 function check(b, x){
   if(b.target.textContent === rightans){
+    soundplay("right");
        x.style.backgroundColor = "green"
       buttons.forEach((x) => {
       if(x.textContent !== rightans){
@@ -61,6 +68,7 @@ function check(b, x){
       }
       })
     } else {
+      soundplay("wrong");
       buttons.forEach((x) => {
       x.style.backgroundColor = "red"
       if(x.textContent === rightans){
@@ -87,4 +95,35 @@ function rhome(){
   triviabox.style.display = "none"
   triviabox.style.animation = "flipIn 1s ease-in";
 
+}
+let audiobtn = document.querySelector("#audio i")
+function sounds(){
+if(sound){
+  sound = false;
+  audiobtn.classList.remove("fa-volume-up");
+  audiobtn.classList.add("fa-volume-off");
+  console.log(audiobtn)
+} else {
+  sound = true;
+  audiobtn.classList.remove("fa-volume-off");
+  audiobtn.classList.add("fa-volume-up");
+  console.log(audiobtn)
+}
+console.log(sound)
+}
+function soundplay(name){
+  if(sound){
+    if(name === "flip"){
+    audio.src = "cardflip.mp3"
+    audio.load();
+    } else if(name === "right"){
+    audio.src = "right.mp3"
+    audio.load();  
+    } else {
+    audio.src = "wrong.mp3"
+    audio.load();
+    }
+  audio.play()
+    
+  }
 }
